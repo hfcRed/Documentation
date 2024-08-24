a0a2bbe9ad69868ae13b84946b83d05dafccbd99
hfcRed <elias.horn@gmx.de>
2024-08-24 12:02:50 +0200

    Create component exports

---
+++ src/components/API/components.js
@@ -0,0 +1,8 @@
+export { default as ApiTester } from './ApiTester.astro';
+export { default as AuthorizationHandler } from './AuthorizationHandler.astro';
+export { default as Button } from './Button.astro';
+export { default as DynamicCode } from './DynamicCode.astro';
+export { default as Input } from './Input.astro';
+export { default as Path } from './Path.astro';
+export { default as Spinner } from './Spinner.astro';
+export { default as Tree } from './Tree.astro';
